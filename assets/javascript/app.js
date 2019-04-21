07abf787941f0f9aa3955cc7a8d88fc4c8de0655
Tamami Polk <tamami_polk@hotmail.com>
2019-04-20 19:15:22 -0500

    Working Version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,13 @@
 var playerConnection = "";
 var myRPSGame;
 var gameConnections;
+var playerOneSelection;
+var playerTwoSelection;
+var winner;
+var playerOneWinningCount = 0;
+var playerTwoWinningCount = 0;
+var imagesShowing =false;
+
 
 // Initialize Firebase
 var config = {
@@ -42,7 +49,7 @@ connectedRef.on("value", function (snap) {
         // Add user to the connections list.
         var con = connectionsRef.push(true);
         playerConnection = con.key;
-        console.log(playerConnection);
+        // console.log(playerConnection);
 
         // Remove user from the connection list when they disconnect.
         con.onDisconnect().remove();
@@ -50,6 +57,72 @@ connectedRef.on("value", function (snap) {
     }
 
 });
+
+// see if players information exists in the database
+database.ref("/playerData").on("value", function (snapshot) {
+    myRPSGame = snapshot.val();
+
+
+// only need to show this once
+        if ((myRPSGame.playerOne != "") && (myRPSGame.playerTwo != "")
+        && (!imagesShowing)) {
+            console.log("show images");
+            $("#messages").empty();
+            showImages();
+            $("#player").addClass("toHide");
+            imagesShowing = true;
+        }
+
+
+            $("#wins-one-count").replaceWith(myRPSGame.playerOneWins);
+            $("#losses-two-count").replaceWith(myRPSGame.playerOneWins);
+            $("#wins-two-count").replaceWith(myRPSGame.playerTwoWins);
+            $("#losses-one-count").replaceWith(myRPSGame.playerTwoWins);
+
+            //     winner = whoWon();
+            //     calculateWinsAndLosses(winner);
+            //
+            // }
+})
+
+
+
+// see if players information exists in the database
+connectionsRef.on("value", function (snap) {
+
+    gameConnections = snap.val();
+    // console.log(gameConnections);
+
+    // console.log("there " + myRPSGame.playerOneKey);
+
+
+    numberOfConnections = snap.numChildren();
+    console.log("number of Connections: " + numberOfConnections);
+
+
+    if (gameConnections[myRPSGame.playerOneKey] == null) {
+        database.ref("/playerData").update({
+            playerOneKey: "",
+            playerOne: "",
+            playerOneWins: 0,
+            playerOneLosses: 0,
+            playerOneSelection: ""
+        });
+
+
+    }
+
+    if (gameConnections[myRPSGame.playerTwoKey] == null) {
+        database.ref("/playerData").update({
+            playerTwoKey: "",
+            playerTwo: "",
+            playerTwoWins: 0,
+            playerTwoLosses: 0,
+            playerTwoSelection: ""
+        });
+
+    }
+})
 
 
 // Initial value
@@ -61,77 +134,246 @@ $("#set-player-button").on("click", function (event) {
     event.preventDefault();
     console.log("clicked the button");
 
+
+// get the name that was entered
     playerName = $("#player-name-input").val().trim();
 
+
+// check if the first player information has been captured
     if (myRPSGame.playerOne === "") {
+
         console.log("value does not exists in playerOne")
 
         database.ref("/playerData").update({
             playerOne: playerName,
             playerOneKey: playerConnection,
             playerOneWins: 0,
-            playerOneLosses: 0
+            playerOneLosses: 0,
+            playerOneSelection: ""
         });
 
-        snapshot.val().playerOne = "tbd"
-    }
+        // myRPSGame.playerOne = playerName;
+        playerName = "";
 
-    if (myRPSGame.playerTwo === "") {
-        console.log("value does not exists in playerTwo")
+    } else if ((myRPSGame.playerTwo === "") && (myRPSGame.playerOne != "")) { // check if the second player's information has been captured
+        console.log("again value does not exists in playerTwo")
 
         database.ref("/playerData").update({
             playerTwoKey: playerConnection,
             playerTwo: playerName,
             playerTwoWins: 0,
-            playerTwoLosses: 0
+            playerTwoLosses: 0,
+            playerTwoSelection: ""
         });
 
-        snapshot.val().playerTwo = "tbd"
+        myRPSGame.playerTwo = playerName;
+        playerName = "";
+
     }
 
+    console.log(myRPSGame.playerOne);
+    console.log(myRPSGame.playerTwo);
 
+
+    if (numberOfConnections > 2) {
+        var message;
+        message = $("#messages").text("Sorry, only two players can play at one time.")
+        message.addClass("messages");
+        $("#messages").append(message);
+    } else if ((myRPSGame.playerOne != "") && (myRPSGame.playerTwo == "")) {
+        var message;
+        message = $("#messages").text("Please wait for your opponent!")
+        message.addClass("messages");
+        $("#messages").append(message);
+    } else if ((myRPSGame.playerTwo != "") && (myRPSGame.playerOne == "")) {
+        var message;
+        message = $("#messages").text("Please wait for your opponent!")
+        message.addClass("messages");
+        $("#messages").append(message);
+    } else if ((myRPSGame.playerTwo != "") && (myRPSGame.playerOne != "")) {
+        console.log("two players");
+    }
 })
 
-// see if players information exists in the database
-database.ref("/playerData").on("value", function (snapshot) {
-        myRPSGame = snapshot.val();
-        console.log("here " + myRPSGame.playerOneKey);
+
+$("#player-images").addClass("toHide");
+$("#score-one").addClass("toHide");
+$("#score-two").addClass("toHide");
+
+
+function showImages() {
+
+    console.log("show images function");
+
+    $("#player-images").addClass("toShow");
+    $("#player-images").removeClass("toHide");
+
+    $("#score-one").addClass("toShow");
+    $("#score-one").removeClass("toHide");
+
+    $("#score-two").addClass("toShow");
+    $("#score-two").removeClass("toHide");
+
+
+    $("#rock").on("click", function (event) {
+
+        console.log("playerConnection = " + playerConnection);
+
+        if (playerConnection === myRPSGame.playerOneKey) {
+            playerOneSelection = "rock";
+            console.log("Player One Rock");
+
+            database.ref("/playerData").update({
+                playerOneSelection: playerOneSelection
+            });
+
+            seeWhoWon();
+
+
+
+        } else if (playerConnection = myRPSGame.playerTwoKey) {
+            playerTwoSelection = "rock";
+            console.log("Player Two Rock");
+
+            database.ref("/playerData").update({
+                playerTwoSelection: playerTwoSelection
+            });
+
+            seeWhoWon();
+
+        }
+
+    })
+
+    $("#paper").on("click", function (event) {
+
+        if (playerConnection === myRPSGame.playerOneKey) {
+            playerOneSelection = "paper";
+            console.log("Player One Paper");
+
+            database.ref("/playerData").update({
+                playerOneSelection: playerOneSelection
+            });
+
+            seeWhoWon();
+
+
+        } else if (playerConnection = myRPSGame.playerTwoKey) {
+            playerTwoSelection = "paper";
+            console.log("Player Two Paper");
+
+            database.ref("/playerData").update({
+                playerTwoSelection: playerTwoSelection
+            });
+
+            seeWhoWon();
+
+
+        }
+
+
+    })
+
+    $("#scissors").on("click", function (event) {
+
+        if (playerConnection === myRPSGame.playerOneKey) {
+            playerOneSelection = "scissors";
+            console.log("Player One Scissors");
+
+            database.ref("/playerData").update({
+                playerOneSelection: playerOneSelection
+            });
+
+            seeWhoWon();
+
+
+        } else if (playerConnection === myRPSGame.playerTwoKey) {
+            playerTwoSelection = "scissors";
+            console.log("Player Two Scissors");
+
+            database.ref("/playerData").update({
+                playerTwoSelection: playerTwoSelection
+            });
+
+            seeWhoWon();
+
+        }
+
+
+    })
+}
+
+function seeWhoWon()
+{
+
+    console.log("Game Begins - check if both have made a selection");
+    if ((myRPSGame.playerOneSelection != "") && (myRPSGame.playerTwoSelection != "")) {
+        winner = whoWon();
+        calculateWinsAndLosses(winner);
+
     }
-)
+}
+
+function whoWon() {
+    console.log("let see who won")
+
+    var result = "nobody";
+
+    console.log("get the results");
+    console.log("playerOne = " + myRPSGame.playerOneSelection);
+    console.log("playerTwo = " + myRPSGame.playerTwoSelection);
 
 
-// see if players information exists in the database
-connectionsRef.on("value", function (snap) {
+    if (((myRPSGame.playerOneSelection == "scissors") && (myRPSGame.playerTwoSelection == "scissors")) ||
+        ((myRPSGame.playerOneSelection == "rock") && (myRPSGame.playerTwoSelection == "rock")) ||
+        ((myRPSGame.playerOneSelection == "paper") && (myRPSGame.playerTwoSelection == "paper"))) {
+        console.log("It was a tie");
+        result = "tie"
+    } else if (((myRPSGame.playerOneSelection == "rock") && (myRPSGame.playerTwoSelection == "scissors")) ||
+        ((myRPSGame.playerOneSelection == "scissors") && (myRPSGame.playerTwoSelection == "paper")) ||
+        ((myRPSGame.playerOneSelection == "paper") && (myRPSGame.playerTwoSelection == "rock"))) {
+        console.log("Player One Won");
+        result = "One"
+    } else if (((myRPSGame.playerTwoSelection == "rock") && (myRPSGame.playerOneSelection == "scissors")) ||
+        ((myRPSGame.playerTwoSelection == "scissors") && (myRPSGame.playerOneSelection == "paper")) ||
+        ((myRPSGame.playerTwoSelection == "paper") && (myRPSGame.playerOneSelection == "rock"))) {
+        console.log("Player Two Won");
+        result = "Two";
+    }
 
-    gameConnections = snap.val();
-    console.log(gameConnections);
+    return result;
 
-    console.log("there " + myRPSGame.playerOneKey);
+}
 
-    if (gameConnections[myRPSGame.playerOneKey] == null) {
+
+function calculateWinsAndLosses(winner) {
+
+    console.log("calculateWinsAndLosses = " + winner);
+
+    if (winner === "One") {
+        console.log("PLAYER ONE WON");
+        playerOneWinningCount = playerOneWinningCount + 1;
         database.ref("/playerData").update({
-            playerOneKey: "",
-            playerOne: "",
-            playerOneWins: 0,
-            playerOneLosses: 0
+            playerOneWins: playerOneWinningCount,
+            playerTwoLosses: playerOneWinningCount
         });
+
     }
 
-    if (gameConnections[myRPSGame.playerTwoKey] == null) {
+    if (winner === "Two") {
+        console.log("PLAYER TWO WON");
+        playerTwoWinningCount = playerTwoWinningCount + 1;
         database.ref("/playerData").update({
-            playerTwoKey: "",
-            playerTwo: "",
-            playerTwoWins: 0,
-            playerTwoLosses: 0
+            playerTwoWins: playerTwoWinningCount,
+            playerOneLosses: playerTwoWinningCount
         });
     }
-})
+
+}
 
 
-$(document).ready(function() {
-    $("#player-images").addClass("tohide");
-    $("#score-one").addClass("tohide");
-    $("#score-two").addClass("tohide");
 
 
-})
+
+
+
